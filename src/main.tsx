812c0d7d070dd731c28707a87e035c5f3be23a0b
Sagar Lahade <sagar.lahade@configserverllp.com>
2026-01-11 21:36:58 +0530

    Fix case-sensitive section component for Linux/Docker 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  //<React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  //</React.StrictMode>
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root container missing in index.html");
+}
+
+ReactDOM.createRoot(rootElement).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
